0b41f90a24643cfa279d69f78531bf0a27be76b8
rjr24 <rezarezaeijamiuk@gmail.com>
2022-04-24 19:22:50 +0100

    userupdate query edited

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-
-mongoose.connect('mongodb://localhost:27017/mongoproject')
-.then(()=> console.log('connected to mongodb'))
-.catch((err)=> console.log('could not connect to mongodb'))
-
+mongoose
+  .connect("mongodb://localhost:27017/mongoproject")
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log("could not connect to mongodb"));
 
 const userSchema = mongoose.Schema({
-  first_name : String,
-  last_name : {type : String , required : true },
-  favorites : [],
-  date : {type : Date , default : Date.now},
-  admin : Boolean
-})
+  first_name: String,
+  last_name: { type: String, required: true },
+  favorites: [],
+  date: { type: Date, default: Date.now },
+  admin: Boolean,
+});
 
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model('User' , userSchema) ;
-
-
-async function createUser(){
+async function createUser() {
   const user = new User({
-    first_name : 'Rostam',
-    last_name : 'jami',
-    favorites : ['sport' , 'data science' , 'music'],
-        admin : false ,
+    first_name: "Rostam",
+    last_name: "jami",
+    favorites: ["sport", "data science", "music"],
+    admin: false,
   });
 
   const result = await user.save();
   console.log(result);
-} 
+}
 
 // createUser();
 
-async function getUsers (){
-  const users = await User.find({last_name : 'jami' , admin : false})
+async function getUsers() {
+  const users = await User.find({ last_name: "jami", admin: false });
   console.log(users);
 }
 
-
 // getUsers();
 
-
-async function updateUsers (id) {
-  const result = await User.update({_id : id}, 
-  {$set : {
-  firstname : 'Boss'
-  }})
-  console.log(result);
+async function updateUser(id) {
+  const user = await User.update(
+    { _id: id },
+    {
+      $set: {
+        firstname: "Boss",
+      },
+    }
+  );
+  console.log(user);
 }
-  
 
-
-updateUsers('62658a31ffd12b3b66635870');
+updateUser("62658a31ffd12b3b66635870");
